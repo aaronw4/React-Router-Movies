dc990684af796e07700bc7008087249b9fa27ecb
Aaron Williams <aaronrw4@yahoo.com>
2019-10-04 19:01:46 -0700

    Added save movie functionality strech goal

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -19,11 +19,10 @@ const Movie = (props) => {
 
   },[id]);
   
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+   const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+   }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -39,7 +38,7 @@ const Movie = (props) => {
       metascore={metascore}
       stars={stars}
       />    
-    <div className="save-button">Save</div>
+    <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
