5bedbfffb0c687148c35f457c6605bb7ffeb72ab
Aaron Williams <aaronrw4@yahoo.com>
2019-10-19 16:32:55 -0700

    Updates

---
+++ precache-manifest.6840233b2391c4ecc88a125a3f63e508.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab7ac38ea39c03bb6e0d3a121aeb8fb",
+    "url": "/React-Router-Apple-Nav/my-app/index.html"
+  },
+  {
+    "revision": "48b10b218b65fbcd5604",
+    "url": "/React-Router-Apple-Nav/my-app/static/css/main.7118d463.chunk.css"
+  },
+  {
+    "revision": "2a46c60f1e1a032a29a3",
+    "url": "/React-Router-Apple-Nav/my-app/static/js/2.ac04cc7a.chunk.js"
+  },
+  {
+    "revision": "48b10b218b65fbcd5604",
+    "url": "/React-Router-Apple-Nav/my-app/static/js/main.2218514f.chunk.js"
+  },
+  {
+    "revision": "b611a0e0be41cee6a576",
+    "url": "/React-Router-Apple-Nav/my-app/static/js/runtime-main.fba5e6f3.js"
+  },
+  {
+    "revision": "6151e109451249027fed98c28139ce7b",
+    "url": "/React-Router-Apple-Nav/my-app/static/media/home.6151e109.jpg"
+  },
+  {
+    "revision": "2521b06de7c0728e16bcd929b66d284c",
+    "url": "/React-Router-Apple-Nav/my-app/static/media/imac.2521b06d.png"
+  },
+  {
+    "revision": "2aafe0dd3e820fe7d81685ea95c82249",
+    "url": "/React-Router-Apple-Nav/my-app/static/media/ipad.2aafe0dd.png"
+  },
+  {
+    "revision": "efea0e7d20e3a08cf007173ba4d96bac",
+    "url": "/React-Router-Apple-Nav/my-app/static/media/iphone.efea0e7d.png"
+  },
+  {
+    "revision": "cb452018f2fb4b027dddc66064298bc5",
+    "url": "/React-Router-Apple-Nav/my-app/static/media/iwatch.cb452018.png"
+  },
+  {
+    "revision": "f07b01e4dfef94cab733d51239ecd586",
+    "url": "/React-Router-Apple-Nav/my-app/static/media/music.f07b01e4.png"
+  },
+  {
+    "revision": "640cded8d64ad1313ac870971bdff584",
+    "url": "/React-Router-Apple-Nav/my-app/static/media/support.640cded8.png"
+  },
+  {
+    "revision": "2885eda880383ea91af3e16ae05731dc",
+    "url": "/React-Router-Apple-Nav/my-app/static/media/tv.2885eda8.png"
+  }
+]);
